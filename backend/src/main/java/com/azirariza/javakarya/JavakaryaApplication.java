7964a1a326b5c8c56be11240df8375b31140d239
azirariza <azirariza9@gmail.com>
2025-06-25 12:30:04 +0700

    123 Go!!!

---
+++ backend/src/main/java/com/azirariza/javakarya/JavakaryaApplication.java
@@ -0,0 +1,13 @@
+package com.azirariza.javakarya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavakaryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavakaryaApplication.class, args);
+	}
+
+}
