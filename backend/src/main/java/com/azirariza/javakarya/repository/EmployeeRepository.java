815419934baba21d15976d28430534101f107351
azirariza <azirariza9@gmail.com>
2025-06-27 05:17:44 +0700

    fix hql error

--- backend/src/main/java/com/azirariza/javakarya/repository/EmployeeRepository.java
+++ backend/src/main/java/com/azirariza/javakarya/repository/EmployeeRepository.java
@@ -9,15 +9,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+
 import com.azirariza.javakarya.entity.Employee;
 
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
-    @Query("SELECT id, name, birth_date, position, id_number, gender,is_delete FROM employee WHERE employee.is_delete = 0")
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+    @Query("SELECT e FROM Employee e WHERE e.isDelete = 0")
     Page<Employee> getListForPagination(Pageable pageable);
 
-    @Query("SELECT id, name, birth_date, position, id_number, gender,is_delete FROM employee WHERE employee.id = :id AND employee.is_delete = 0")
-    Optional<Employee> getActiveById (@Param("id") int id);
+    @Query("SELECT e FROM Employee e WHERE e.id = :id AND e.isDelete = 0")
+    Optional<Employee> getActiveById(@Param("id") int id);
 
 }
--- backend/src/main/java/com/azirariza/javakarya/repository/PositionRepository.java
+++ backend/src/main/java/com/azirariza/javakarya/repository/PositionRepository.java
@@ -14,7 +14,7 @@ import com.azirariza.javakarya.entity.Position;
 
 @Repository
 public interface PositionRepository extends JpaRepository<Position,Position> {
-    @Query("SELECT id, code, name,is_delete FROM position WHERE position.is_delete = 0")
+    @Query("SELECT p FROM Position p WHERE p.isDelete = 0")
     List<Position> getList();
 
 }
